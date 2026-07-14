6c609a40517c6c70a029d46cf5ae178ba6541d4d
LuisCarlosCarvalho <121337847+LuisCarlosCarvalho@users.noreply.github.com>
2026-07-14 09:25:59 +0000

    Deploying to deploy from @ LuisCarlosCarvalho/umbulab@560724d5d689af836fccbb464db06a9f39120446 🚀

--- assets/arrow-left-DQyr4fLP-1784020075086.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./index-Qh5okDav-1784020075086.js";const t=e("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);export{t as A};
---
+++ assets/chevron-right-BvYLBLvT-1784021150143.js
@@ -0,0 +1,1 @@
+import{c as t}from"./index-gFUr5h4G-1784021150143.js";const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
