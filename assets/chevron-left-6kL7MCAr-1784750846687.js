6f7b7e15fb1f74bd01b24818f8f3c5eda0531173
LuisCarlosCarvalho <121337847+LuisCarlosCarvalho@users.noreply.github.com>
2026-07-22 20:07:35 +0000

    Deploying to deploy from @ LuisCarlosCarvalho/umbulab@0ac1e95553908ed8a98b9fbb80d3391e2377fb55 🚀

---
+++ assets/chevron-left-6kL7MCAr-1784750846687.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-DlAKjaOl-1784750846687.js";const t=e("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
---
+++ assets/chevron-right-DXCEBXB2-1784750846687.js
@@ -0,0 +1,1 @@
+import{c as t}from"./index-DlAKjaOl-1784750846687.js";const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
